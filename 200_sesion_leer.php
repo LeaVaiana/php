088f0c6c4d17207f629eeb53527ae419ac294caf
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2019-03-10 17:00:34 +0100

    leer variable de sesion, otro archivo

--- 200_sesion_leer.php
+++ 200_sesion_leer.php
@@ -2,7 +2,6 @@
     
     session_start();//esta variable de sesion ha sido creada en el ejercicio anterior, para ver que la variable de sesion se puede leer en otra pagina siembre y cuando esté en el mismo servidor
 
-
     if(isset($_SESSION['sesion'])){
         extract($_SESSION['sesion']);
         echo "$a $b";
